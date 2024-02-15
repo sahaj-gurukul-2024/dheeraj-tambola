8db2751bcc0cb64802b3bc9ac90f68834af5ac70
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-15 10:57:49 +0530

    eary five validation done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -50,7 +50,26 @@ fun validateClaim(ticket: List<List<Int>>, announcedNumbers: List<Int>, claim: C
             res
         }
         Claim.EARLY_FIVE -> {
-            true
+            val row = mutableSetOf<Int>()
+            for(i in ticket.indices) {
+                for(j in ticket[i].indices) {
+                    if(ticket[i][j] != -1) {
+                        row.add(ticket[i][j])
+                    }
+                }
+            }
+            var res = false
+            var itemsRem = 0
+            for(i in announcedNumbers.indices) {
+                if(row.contains(announcedNumbers[i])) {
+                    itemsRem++
+                    if(itemsRem == 5) {
+                        res = i == announcedNumbers.size -1
+                    }
+                }
+            }
+
+            res
         }
     }
 
