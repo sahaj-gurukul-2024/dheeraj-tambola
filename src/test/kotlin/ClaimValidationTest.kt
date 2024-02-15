a212bbab5e87518a6ff9bcb9531ed106c2c41b14
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-15 10:43:11 +0530

    added tests for bottom row

--- src/test/kotlin/ClaimValidationTest.kt
+++ src/test/kotlin/ClaimValidationTest.kt
@@ -35,4 +35,34 @@ class ClaimValidationTest {
 
         assertFalse(result)
     }
+
+    @Test
+    fun `check if bottom row wins`() {
+        val ticket = listOf(
+            listOf(4,16,-1,-1,48,-1,63,76,-1),
+            listOf(7,-1,23,38,-1,52,-1,-1,80),
+            listOf(9,-1,25,-1,-1,56,64,-1,83)
+        )
+        val announcedNumbers = listOf(9, 25, 46, 56, 89, 64, 76, 83)
+        val claim = Claim.BOTTOM_ROW
+
+        val result = validateClaim(ticket, announcedNumbers, claim)
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun `check if bottom row losses`() {
+        val ticket = listOf(
+            listOf(4,16,-1,-1,48,-1,63,76,-1),
+            listOf(7,-1,23,38,-1,52,-1,-1,80),
+            listOf(9,-1,25,-1,-1,56,64,-1,83)
+        )
+        val announcedNumbers = listOf(9, 25, 83, 56, 89, 64, 76, 8)
+        val claim = Claim.BOTTOM_ROW
+
+        val result = validateClaim(ticket, announcedNumbers, claim)
+
+        assertFalse(result)
+    }
 }
