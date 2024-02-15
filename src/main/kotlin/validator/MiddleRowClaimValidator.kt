d9d9e4c876f67b44584832635a7f4dcb249a2861
Nagarjunan G <nagarjunang@Nagarjunans-MacBook-Pro.local>
2024-02-15 19:45:23 +0530

    Simplify logic of evaluating result

--- src/main/kotlin/validator/MiddleRowClaimValidator.kt
+++ src/main/kotlin/validator/MiddleRowClaimValidator.kt
@@ -5,21 +5,17 @@ import org.example.Claim
 class MiddleRowClaimValidator: ClaimValidator {
     override fun validate(ticket: List<List<Int>>, announcedNumbers: List<Int>, claim: Claim): Boolean {
         val row = mutableSetOf<Int>()
-        for(i in ticket[ticket.size -2]) {
-            if(i != -1) {
-                row.add(i)
+        for(item in ticket[ticket.size -2]) {
+            if(item != -1) {
+                row.add(item)
             }
         }
         var result = false
-        for (index in announcedNumbers.indices) {
-            if (row.contains(announcedNumbers[index])){
-                for(i in announcedNumbers.indices) {
-                    if(row.contains(announcedNumbers[i])) {
-                        row.remove(announcedNumbers[i])
-                        if(row.isEmpty()) {
-                            result = i == announcedNumbers.size -1
-                        }
-                    }
+        for(index in announcedNumbers.indices) {
+            if(row.contains(announcedNumbers[index])) {
+                row.remove(announcedNumbers[index])
+                if(row.isEmpty()) {
+                    result = index == announcedNumbers.size -1
                 }
             }
         }
