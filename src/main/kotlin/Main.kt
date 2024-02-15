a808f6a6a4b4cb96db98e8809ff7e18fdd73a7a7
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-15 10:44:29 +0530

    done validation for bottom row

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -30,7 +30,24 @@ fun validateClaim(ticket: List<List<Int>>, announcedNumbers: List<Int>, claim: C
             res
         }
         Claim.BOTTOM_ROW -> {
-            false
+            val row = mutableSetOf<Int>()
+            for(i in ticket[ticket.size -1]) {
+                if(i != -1) {
+                    row.add(i)
+                }
+            }
+            var res = false
+            for(i in announcedNumbers.indices) {
+                if(row.contains(announcedNumbers[i])) {
+                    row.remove(announcedNumbers[i])
+                    println(row)
+                    if(row.isEmpty()) {
+                        res = i == announcedNumbers.size -1
+                    }
+                }
+            }
+
+            res
         }
         Claim.EARLY_FIVE -> {
             true
