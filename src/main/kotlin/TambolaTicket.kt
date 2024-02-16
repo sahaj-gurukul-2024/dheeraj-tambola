fa69faed3afbe387e37fdb5ee52b48d03b1c8a84
Nagarjunan G <nagarjunang@Nagarjunans-MacBook-Pro.local>
2024-02-16 16:25:27 +0530

    Create an object for tambola ticket

---
+++ src/main/kotlin/TambolaTicket.kt
@@ -0,0 +1,17 @@
+package org.example
+
+class TambolaTicket(
+    topRow: List<Int>,
+    middleRow: List<Int>,
+    bottomRow: List<Int>
+) {
+    val topRow = topRow
+    val middleRow = middleRow
+    val bottomRow = bottomRow
+
+    fun getFullTicket(): List<Int> {
+
+        return this.topRow + this.middleRow + this.bottomRow
+
+    }
+}
